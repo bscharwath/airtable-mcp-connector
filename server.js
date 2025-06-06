01cecd5daad1c580f34ddf780fafd36b0227be77
bscharwath <beth@bethscharwath.com>
2025-06-06 12:29:10 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const path = require('path');
-
-app.use('/.well-known', express.static(path.join(__dirname, '.well-known')));
-
 const axios = require('axios');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
